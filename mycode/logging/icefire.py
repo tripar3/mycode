60a4fbab1e57c02372ed27d5a54eba472d0267ba
tripar3 <tripar3@users.noreply.github.com>
2022-11-11 05:00:14 +0000

    Ice and Fire API with logging.

---
+++ mycode/logging/icefire.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""RZFeeser | Alta3 Research
+Exploring logging options within python"""
+
+# standard library imports
+import logging
+import argparse
+import pprint
+
+# python3 -m pip install requests
+import requests
+
+BOOK = "https://www.anapioficeandfire.com/api/books"
+
+def main():
+
+    # if you use logging, always describe your logging.basicConfig @ the start
+    # default logging level is 'warning', which would skip less severe warnings
+    logging.basicConfig(filename='icefire.log', format='%(levelname)s:%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+    
+    try:
+        # this write
+        # INFO: 12/12/2019 11:55:02 AM Program started
+        logging.info('Scripting started')
+    
+        # make a call to the API
+        icefire = requests.get(BOOK + "/" + args.bookno)
+       
+        # force a ZeroDivisionError
+        # 10 / 0
+
+        # pretty print the json response
+        pprint.pprint(icefire.json())
+        
+        # write response code to log
+        logging.info("API Response Code - " + str(icefire))
+        
+    # if a program errors, write that error to a log file
+    except Exception as err:
+        logging.critical(err)
+                        
+    finally:
+        # INFO: 12/12/2019 11:55:02 AM Program ended
+        logging.info("Program ended")
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bookno', help='Enter the book number (integer) to look up.')
+    args = parser.parse_args()
+    main()
+
