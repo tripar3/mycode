a18f0b81a5b5138f4b4ffac039c7cf9835ae85a8
tripar3 <me.raghvendra@live.in>
2023-01-03 22:00:12 +0530

    Script added demonstrate proficiency with the requests HTTP library Ice & Fire API HTTP response parsing

--- alta3research-mycode-cert/alta3research-requests02.py
+++ alta3research-mycode-cert/alta3research-requests02.py
@@ -2,25 +2,22 @@
 """Script to demonstrate proficiency with the requests HTTP library
    Ice & Fire API HTTP response parsing"""
 
-# pprint (pretty printer) makes Python data structure a lot more human readable
-from pprint import pprint
-
 # requests is used to send HTTP requests
-import requests, sys
+import requests
 
-# append the command line argument to the URL
-URL= "https://anapioficeandfire.com/api/characters/"+str(sys.argv[1])
-
-def main(argv):
+def main():
     """sending GET request and checking response"""
 
+    nu= input("Enter any numeric value to get the 'Game of Thrones' character's name and its real name. E.g. 583 > ")
+    # append the command line input to create URL
+    URL= "https://anapioficeandfire.com/api/characters/"+str(nu)
     # URL response is stored in "resp" object
     resp= requests.get(URL)
 
     # check to see if the status is a 200 OK
     if resp.status_code == 200:
         # verify if the URL parsed is valid or not
-        print("Response code is " + str(resp.status_code) + " . It is valid URL.", "\n")
+        print("Response code is " + str(resp.status_code) + ". It is valid URL.", "\n")
         # convert the JSON content of the response into a python dictionary
         res= resp.json()
         # check if the character exists
@@ -32,10 +29,8 @@ def main(argv):
                 print(resp.json()["name"] + " character is played by Unknown Actor.\n")
         else:
             print("No character found.\n")
-        pprint(res)
     else:
         print("Invalid URL.", resp.reason, resp.status_code)
 
 if __name__ == "__main__":
-    # Pass the number as argument to get character and its real name
-    main(sys.argv[1:])
+    main()
